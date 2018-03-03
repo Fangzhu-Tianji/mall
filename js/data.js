04672d983668b1ea7f13af9d5541976674f4c304
Fangzhu-Tianji <zch793262201@163.com>
2018-03-03 16:25:39 +0800

    add

---
+++ js/data.js
@@ -0,0 +1,11 @@
+var a = [
+    'http://pic1.win4000.com/wallpaper/2017-12-19/5a387cb8439ea.jpg',
+    'http://pic1.win4000.com/wallpaper/2017-12-19/5a387cbbd1eae.jpg',
+    'http://pic1.win4000.com/wallpaper/2017-12-19/5a387cbfa8b45.jpg',
+    'http://pic1.win4000.com/wallpaper/2017-12-19/5a387cc3af0c0.jpg',
+    'http://pic1.win4000.com/wallpaper/2017-12-19/5a387cdbbdfe0.jpg',
+    'http://pic1.win4000.com/wallpaper/2017-12-19/5a387ce416877.jpg',
+    'http://pic1.win4000.com/wallpaper/2017-12-19/5a387cfd18684.jpg',
+    'http://pic1.win4000.com/wallpaper/2017-12-19/5a387d001eb67.jpg',
+    'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3383789762,960249868&fm=27&gp=0.jpg'
+]
