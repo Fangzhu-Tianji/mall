f2fa2fc59fce3687c3448c5ac556d5a3bbfb1b65
Fangzhu-Tianji <zch793262201@163.com>
2018-03-07 15:20:28 +0800

    add

--- data/index.js
+++ data/index.js
@@ -9,18 +9,93 @@ var navigation = {
                         content: ['Dresses','Hoodies & Sweatshirts','Blouses & Shirts','T-Shirts','Sweaters','Socks & Hosiery']
                     },
                     {
-                        tit: 'Hot Categories',
-                        content: ['Dresses','Hoodies & Sweatshirts','Blouses & Shirts','T-Shirts','Sweaters','Socks & Hosiery']
+                        tit: 'Outwear & Jackets',
+                        content: ['Basic Jackets','Real Fur','Down Coats','Blazers','Trench','Parkas']
                     }
                 ],
+                [
+                    {
+                        tit: 'Tops & Sets',
+                        content: ['Tank Tops','Suits & Sets','Jumpsuits','Rompers','Intimates','Sleep & Lounge']
+                    },
+                    {
+                        tit: 'Weddings & Events',
+                        content: ['Wedding Dresses','Evening Dresses','Prom Dresses','Bridesmaid Dresses','Flower Girl Dresses','Cocktail Dresses']
+                    }
+                ]
+            ]
+        },
+        {
+            tltle: ['Men’s Clothing'],
+            boxOut: [
+                [
+                    {
+                        tit: 'Hot Sale',
+                        content: ['Hoodies & Sweatshirts','Jackets','T-Shirts','Shirts','Sweaters','Socks']
+                    },
+                    {
+                        tit: 'Bottoms',
+                        content: ['Casual Pants','Cargo Pants','Jeans','Sweatpants','Harem Pants','Shorts']
+                    }
+                ],
+                [
+                    {
+                        tit: 'Outerwear & Jackets',
+                        content: ['Trench','Genuine Leather','Parkas','Down Jackets','Wool & Blends','Suits & Blazer']
+                    },
+                    {
+                        tit: 'Underwear & Loungewear',
+                        content: ['Boxers','Briefs','Long Johns','Mens Sleep & Lounge','Pajama Sets','Robes']
+                    }
+                ]
+            ]
+        },
+        {
+            tltle: ['Women’s Clothing'],
+            boxOut: [
                 [
                     {
                         tit: 'Hot Categories',
                         content: ['Dresses','Hoodies & Sweatshirts','Blouses & Shirts','T-Shirts','Sweaters','Socks & Hosiery']
                     },
                     {
-                        tit: 'Hot Categories',
-                        content: ['Dresses','Hoodies & Sweatshirts','Blouses & Shirts','T-Shirts','Sweaters','Socks & Hosiery']
+                        tit: 'Outwear & Jackets',
+                        content: ['Basic Jackets','Real Fur','Down Coats','Blazers','Trench','Parkas']
+                    }
+                ],
+                [
+                    {
+                        tit: 'Tops & Sets',
+                        content: ['Tank Tops','Suits & Sets','Jumpsuits','Rompers','Intimates','Sleep & Lounge']
+                    },
+                    {
+                        tit: 'Weddings & Events',
+                        content: ['Wedding Dresses','Evening Dresses','Prom Dresses','Bridesmaid Dresses','Flower Girl Dresses','Cocktail Dresses']
+                    }
+                ]
+            ]
+        },
+        {
+            tltle: ['Men’s Clothing'],
+            boxOut: [
+                [
+                    {
+                        tit: 'Hot Sale',
+                        content: ['Hoodies & Sweatshirts','Jackets','T-Shirts','Shirts','Sweaters','Socks']
+                    },
+                    {
+                        tit: 'Bottoms',
+                        content: ['Casual Pants','Cargo Pants','Jeans','Sweatpants','Harem Pants','Shorts']
+                    }
+                ],
+                [
+                    {
+                        tit: 'Outerwear & Jackets',
+                        content: ['Trench','Genuine Leather','Parkas','Down Jackets','Wool & Blends','Suits & Blazer']
+                    },
+                    {
+                        tit: 'Underwear & Loungewear',
+                        content: ['Boxers','Briefs','Long Johns','Mens Sleep & Lounge','Pajama Sets','Robes']
                     }
                 ]
             ]
